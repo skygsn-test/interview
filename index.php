18bbb104f2ea5ecdc149df473af531f94833f356
skygsn-test <skygsn@gmail.com>
2015-07-21 23:20:30 +0300

    Считывание CSV файла в массив и вывод строк со страной = RU

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+if ($csvFile = fopen('contacts.csv', 'r')) {
+    $countryCodeCol = 0;
+    $i = 0;
+
+    while (($csvData = fgetcsv($csvFile, 0, ';')) !== false) {
+        if ($i == 0) {
+            $countryCodeCol = array_search('Страна', $csvData);
+        } else {
+            if ($csvData[$countryCodeCol] == 'RU') {
+                echo implode(', ', $csvData) . "\n";
+            }
+        }
+
+        $i++;
+    }
+} else {
+    echo 'Файл не найден';
+}
